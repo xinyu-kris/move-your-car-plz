6f7de278bb3cbe53d7472c723cd49f4bd3a78837
kris <945729058@qq.com>
2025-06-04 22:09:33 +0800

    fix: fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: "/move-your-car-plz/",
     resolve: {
         alias: {
             "@": resolve(__dirname, "./src"),
